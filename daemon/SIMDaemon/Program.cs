ff457c42ad1e215a3c59041eb39796960f09777b
Verox <verox.averre@gmail.com>
2015-06-18 23:41:16 +0100

    Removed conflicting constant.

--- daemon/SIMDaemon/Program.cs
+++ daemon/SIMDaemon/Program.cs
@@ -13,7 +13,6 @@ namespace SIMDaemon
 {
     class Program
     {
-        const string unique_pipe_name = "SIMSRV1";
         const string unique_server_name = "SRV1";
         const string unique_pipe_name = "SIM" + unique_server_name;
 
