26501b04dfc0b250dca35aafaf2d01f2215528c5
Verox <verox.averre@gmail.com>
2015-07-13 18:33:27 +0100

    Incomming data now scrubs kills for duplicates.

--- web/aar/api/v1/data.php
+++ web/aar/api/v1/data.php
@@ -18,6 +18,30 @@ $json = file_get_contents("php://input"); // What the fuck, php...
 
 // Decode the json.
 $decoded_json = json_decode($json, true);
+
+// Clean up double-firing kills events.
+/**
+ * I'm not sure if the name field is sufficently unique to be using to filter duplicate events
+ * Ideally, nid should be used as that is guarenteed unique by it's nature, but i'm unsure if
+ * that will remain constant as the unit dies.
+ */
+if (count($decoded_json['data']['kills']) > 1) // If we only have 1 kill event then there's no need to clean double-fired events.
+{
+    $victims = array();
+
+    foreach ($decoded_json['data']['kills'] as $k_key => $k_event)
+    {
+        if (in_array($k_event['victim']['name'],$victims))
+        {
+            unset($decoded_json['data']['kills'][$k_key]); // Trash the duplicate.
+        }
+        else
+        {
+            array_push($victims, $k_event['victim']['name']);
+        }
+    }
+}
+
 print_r($decoded_json);
 
 // Init the live cache handler and perform the update.
